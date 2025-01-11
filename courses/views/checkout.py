bb8f46d62e66be3d154147de461f90a460de555f
Your Name <your.email@example.com>
2025-01-11 21:14:01 +0530

    now removing the show more button if payment is done

--- courses/views/checkout.py
+++ courses/views/checkout.py
@@ -1,7 +1,7 @@
 import datetime
 from time import time
 from django.shortcuts import render,redirect
-from courses.models import Course,Video,Payment
+from courses.models import Course,Video,Payment,UserCourse
 from project.settings import *
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
@@ -12,7 +12,7 @@ client = razorpay.Client(auth=(KEY_ID,KEY_SECRET))
 
 
 def checkout(request,slug):
-    course = Course.objects.get(slug=slug)# pylint: disable=no-member
+    course = Course.objects.get(slug=slug)
     user = None
     if request.user.is_authenticated is False:
         return redirect('login')
@@ -21,33 +21,43 @@ def checkout(request,slug):
     action=request.GET.get('action')
     order=None
     payment = None
+    error = None
     if action=='create_payment':
-        amount = int((course.price - (course.price * course.discount * 0.01))*100)
-        currency = 'INR'
-        notes = {
-            'email':user.email,
-            'name': f'{user.first_name} {user.last_name}'
-        }
+        try:
+            user_course = UserCourse.objects.get(user = user,course = course)# pylint: disable=no-member
+            error = 'you are already enrolled in this course'
+        except:
+            pass 
+        
+        if error is None:
+            amount = int((course.price - (course.price * course.discount * 0.01))*100)
+            currency = 'INR'
+            notes = {
+                'email':user.email,
+                'name': f'{user.first_name} {user.last_name}'
+            }
 
-        reciept = f'gurukul-{int(time())}'
-        order = client.order.create(# pylint: disable=no-member
-            {'receipt':reciept,
-            'notes':notes,
-            'amount':amount,
-             'currency':currency})
+            reciept = f'gurukul-{int(time())}'
+            order = client.order.create(# pylint: disable=no-member
+                {'receipt':reciept,
+                'notes':notes,
+                'amount':amount,
+                'currency':currency})
 
-        payment = Payment()
-        payment.user = user
-        payment.course = course
-        payment.order_id = order.get('id')
-        payment.save()
+            payment = Payment()
+            payment.user = user
+            payment.course = course
+            payment.order_id = order.get('id')
+            payment.save()
 
 
     context = {
         'course':course,
         'order':order,
         'payment':payment,
-        'user':user
+        'user':user,
+        'error':error
+
     }
     
     return render(request,'courses/check-out.html',context)
@@ -56,6 +66,7 @@ a=time()
 print(a)
 # print('ygdjkhfsdshfuisdf',time())
 
+
 @csrf_exempt
 def verifyPayment(request):
     if request.method == 'POST':
@@ -63,8 +74,23 @@ def verifyPayment(request):
         context = {}
         try:
             client.utility.verify_payment_signature(data)# pylint: disable=no-member
+            razorpay_order_id = data['razorpay_order_id']
+            razorpay_payment_id = data['razorpay_payment_id']
+            payment = Payment.objects.get(order_id = razorpay_order_id)# pylint: disable=no-member
+            payment.payment_id = razorpay_payment_id 
+            payment.status = True 
+
+            userCourse = UserCourse(user = payment.user,course = payment.course)
+            userCourse.save()
+
+            payment.user_course = userCourse
+            payment.save()
+
+
             return render(request, template_name='courses/my_courses.html',context={})
         except:
             return HttpResponse('Invalid Payment Details')
+
+
 
 
